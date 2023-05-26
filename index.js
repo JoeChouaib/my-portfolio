ce3d0f96a59b7204d09276671d6f596e2241a889
unknown <chouaibfadli077@gmail.com>
2023-05-26 09:36:00 +0100

    adding scroll progress commit

--- index.js
+++ index.js
@@ -21,3 +21,26 @@ window.onload = function () {
     location.reload();
   });
 };
+
+window.addEventListener("scroll", function () {
+  var isNavbarTogglerCollapsed = document
+    .querySelector(".navbar-toggler")
+    .getAttribute("aria-expanded");
+
+  if (isNavbarTogglerCollapsed === "true") {
+    document.querySelector(".navbar-toggler").click();
+  }
+});
+
+function progressBarScroll() {
+  let winScroll = document.body.scrollTop || document.documentElement.scrollTop,
+    height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight,
+    scrolled = (winScroll / height) * 100;
+  document.getElementById("progressBar").style.width = scrolled + "%";
+}
+
+window.onscroll = function () {
+  progressBarScroll();
+};
